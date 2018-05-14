d23f41927bf99e8cd77cb1f66af2072756991f05
Hans Lauwers <hans.lauwers@hotmail.com>
2018-05-14 18:31:38 +0000

    Add AngularJS and init app

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,13 +9,23 @@ class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     begin 
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      
-    
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+      return nil unless looked_up_stock.company_name
+      new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.company_name)
+      new_stock.last_price = new_stock.price
+      new_stock
     rescue Exception => e 
       return nil
     end
+  end
+  
+  def price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+    
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+    
+    "Unavailable"
   end
   
   def self.strip_commas(number)
